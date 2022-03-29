1e7d6860f3a86ae602a79dcaa75b58adfbbe8c3f
Manny Lee <many221@gmail.com>
2022-03-28 19:36:42 -0700

    get,post,put done

--- src/main/java/go_rest_api_v12/go_rest_api_v12/controllers/UserController.java
+++ src/main/java/go_rest_api_v12/go_rest_api_v12/controllers/UserController.java
@@ -1,11 +1,10 @@
 package go_rest_api_v12.go_rest_api_v12.controllers;
 
 import go_rest_api_v12.go_rest_api_v12.models.UserModel;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -16,18 +15,39 @@ import java.util.Objects;
 @RequestMapping("/api/user")
 public class UserController {
 
+    private final String URL = "https://gorest.co.in/public/v2/users";
+
+    @Autowired
+    private Environment env;
+
+
+    //X--> Get Requests
+
+    @GetMapping("/{id}")
+    public ResponseEntity< UserModel> getUserByID(@PathVariable int id, RestTemplate restTemplate){
+
+        try {
+            String userUrl = URL + "/" + id;
+
+            return new ResponseEntity<> ( restTemplate.getForObject ( userUrl, UserModel.class ), HttpStatus.OK );
+
+        } catch (Exception e){
+
+            System.out.println (e.getClass ());
+            System.out.println (e.getMessage ());
+
+            return new ResponseEntity( e.getMessage (), HttpStatus.INTERNAL_SERVER_ERROR );
+        }
+    }
 
     @GetMapping("/all")
     public ResponseEntity getAllUser( RestTemplate restTemplate ){
 
-
         try {
-
-            String url = "https://gorest.co.in/public/v2/users";
 
             ArrayList<UserModel> allUserArray = new ArrayList<>();
 
-            ResponseEntity<UserModel[]> response = restTemplate.getForEntity( url, UserModel[].class );
+            ResponseEntity<UserModel[]> response = restTemplate.getForEntity( URL, UserModel[].class );
 
             allUserArray.addAll ( Arrays.asList ( Objects.requireNonNull ( response.getBody () ) ) );
 
@@ -36,27 +56,123 @@ public class UserController {
                             response.getHeaders ().get (
                                     "X-Pagination-Pages" ) ).get ( 0 ) );
 
-            for (int i = 2; i < totalPageNumber; i++) {
+            for (int i = 2; i <= 5; i++) {
 
-                String tempURl = url + "?page=" + i;
+                String tempURl = URL + "?page=" + i;
 
                 UserModel[] pageData = restTemplate.getForObject ( tempURl,UserModel[].class );
 
                 assert pageData != null;
+
                 allUserArray.addAll ( Arrays.asList ( pageData));
 
             }
+
+            System.out.println (allUserArray.size ());
 
             return new ResponseEntity ( allUserArray,HttpStatus.OK );
 
         } catch( Exception e ){
 
-            System.out.println (e.getClass ()) ;
+            System.out.println (e.getClass ());
+            System.out.println (e.getMessage ());
 
             return new ResponseEntity( e.getMessage (), HttpStatus.INTERNAL_SERVER_ERROR );
 
         }
 
     }
+
+    //X--> Post Requests
+
+    @PostMapping
+    public ResponseEntity<UserModel> createUser(@RequestBody UserModel newUser, RestTemplate restTemplate){
+
+        try {
+
+            String token = env.getProperty ( "GO_REST_API_KEY" );
+
+            //X| Through URl
+
+//            String postURl = URL + "?access-token=" + token;
+
+            //X|
+
+            //O| Through Headers
+
+            HttpHeaders headers = new HttpHeaders ();
+
+            headers.setBearerAuth ( token );
+
+//            headers.set ( "Authorization", "Bearer " + token );
+
+            //O|
+
+            HttpEntity<UserModel> request = new HttpEntity<UserModel> ( newUser,headers );
+
+            UserModel response = restTemplate.postForObject ( URL,request,UserModel.class );
+
+            return new ResponseEntity<> (response,HttpStatus.CREATED );
+
+        } catch (Exception e){
+
+            System.out.println (e.getClass ());
+
+            System.out.println (e.getMessage ());
+
+            return new ResponseEntity( e.getMessage (), HttpStatus.INTERNAL_SERVER_ERROR );
+
+        }
+
+    }
+
+    //X--> Put Requests
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserModel> updateUser(@PathVariable int id,@RequestBody UserModel updateData,RestTemplate restTemplate){
+
+        try {
+
+            String userUrl = URL + "/"+ id;
+
+            HttpEntity<UserModel> request = new HttpEntity<> ( updateData );
+
+            ResponseEntity<UserModel> response =  restTemplate.exchange ( userUrl, HttpMethod.PUT,request,UserModel.class );
+
+            return new ResponseEntity<UserModel>(response.getBody (),HttpStatus.OK);
+
+        } catch (Exception e){
+
+            System.out.println (e.getClass ());
+
+            System.out.println (e.getMessage ());
+
+            return new ResponseEntity( e.getMessage (), HttpStatus.INTERNAL_SERVER_ERROR );
+        }
+    }
+
+    //X--> Delete Requests
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserModel> deleteUser(@PathVariable int id, RestTemplate restTemplate){
+
+        try {
+
+            String deletedUser = URL +"/"+id;
+
+            return restTemplate.delete ( deletedUser,UserModel.class );
+
+        } catch (Exception e){
+
+            System.out.println (e.getClass ());
+
+            System.out.println (e.getMessage ());
+
+            return new ResponseEntity( e.getMessage (), HttpStatus.INTERNAL_SERVER_ERROR );
+
+        }
+
+    }
+
 
 }
